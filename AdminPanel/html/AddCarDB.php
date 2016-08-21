82ccc28e321a29d04ed92b93be5b15eb8859bbca
rohitanil <rohitanil7121993@gmail.com>
2016-08-21 14:46:07 +0530

    Update car add feature

--- AdminPanel/html/AddCarDB.php
+++ AdminPanel/html/AddCarDB.php
@@ -11,6 +11,15 @@ $car_torq=$_POST['cartorque'];
 $c0to100=$_POST['c0to100'];
 $rat=$_POST['rat_url'];
 
+$fcap=$_POST['fcapacity'];
+$ftype=$_POST['ftype'];
+$seat=$_POST['seat'];
+$transtype=$_POST['trantype'];
+$edisp=$_POST['edisp'];
+$topspeed=$_POST['topspeed'];
+$edesc=$_POST['edesc'];
+$etype=$_POST['etype'];
+
 
 $con=mysqli_connect('localhost','root','','carhunt');
 if(!$con)
@@ -18,10 +27,10 @@ if(!$con)
 		echo "connection failed";
 		die();
 	}
-	elseif((!empty($car_mod))&&(!empty($car_type))&&(!empty($car_brand))&&(!empty($car_price))&&(!empty($rURL))&&(!empty($iURL))&&(!empty($car_mile))&&(!empty($car_torq))&&(!empty($c0to100)))
+	elseif((!empty($fcap))&&(!empty($ftype))&&(!empty($seat))&&(!empty($transtype))&&(!empty($edisp))&&(!empty($topspeed))&&(!empty($edesc))&&(!empty($etype))&&(!empty($car_mod))&&(!empty($car_type))&&(!empty($car_brand))&&(!empty($car_price))&&(!empty($rURL))&&(!empty($iURL))&&(!empty($car_mile))&&(!empty($car_torq))&&(!empty($c0to100)))
 	{
 	  mysqli_select_db($con,"car");
-      $query1="INSERT INTO car  (carid,type,brand,price,rating,review_url,image_url,mileage,torq,c0to100) VALUES ('$car_mod','$car_type','$car_brand','$car_price','$rat','$rURL','$iURL','$car_mile','$car_torq','$c0to100')";
+      $query1="INSERT INTO car  (carid,type,brand,price,rating,review_url,image_url,mileage,torq,c0to100,fcap,ftype,seat,transtype,edisp,topspeed,edesc,etype) VALUES ('$car_mod','$car_type','$car_brand','$car_price','$rat','$rURL','$iURL','$car_mile','$car_torq','$c0to100','$fcap','$ftype','$seat','$transtype','$edisp','$topspeed','$edesc','$etype')";
       if(mysqli_query($con,$query1))
       { 
       	session_start();
