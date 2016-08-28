5b645e94b04ba7a37b7a414a5841f200238161c2
au-revoir <govindg0204@gmail.com>
2016-08-28 15:28:09 +0530

    Edit database credentials

--- UserPanel/getter.php
+++ UserPanel/getter.php
@@ -1,32 +1,9 @@
 <?php
 
-$con=mysqli_connect('localhost','root','','carhunt');
+$con=mysqli_connect('localhost','root','ezdine@123','carhunt');
 $selected=mysqli_select_db($con,"car");
 $dept = $_GET["first-choice"];
-/*if($dept == "R")
-{
-	$dbselected='r_subjectlist';
-}
-if($dept == "M")
-{
-	$dbselected='m_subjectlist';
-}
-if($dept == "P")
-{
-	$dbselected='p_subjectlist';
-}
-if($dept == "U")
-{
-	$dbselected='u_subjectlist';
-}
-if($dept == "B")
-{
-	$dbselected='b_subjectlist';
-}
-if($dept == "T")
-{
-	$dbselected='t_subjectlist';
-}*/
+
 $choice=$_GET["second-choice"];
 $query="SELECT `carid` FROM `car` WHERE `brand`='$choice' AND `type`='$dept'";
 $result=mysqli_query($con, $query);
